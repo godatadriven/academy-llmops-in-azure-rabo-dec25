1b87bbf8ede5424cd36f31953d5bb4481b87a5ce
Jasper Krebbers <jasper.krebbers@rabobank.nl>
2025-12-15 13:33:06 +0100

    start ex6

--- tests/test_evaluation.py
+++ tests/test_evaluation.py
@@ -13,5 +13,10 @@ configure_tracer()
 def evaluation_data() -> pd.DataFrame:
     return get_evaluation_data()
 
-
+def test_run_evaluation(evaluation_data: pd.DataFrame):
+    results = run_evaluation(evaluation_data)
+    assert results["general_info_success_rate"] >= 0.8
+    assert results["title_accuracy"] >= 0.5
+    assert results["business_classification_accuracy"] >= 0.7
+    assert results["summarization_rouge_1"] >= 0.15    # TODO: Fill me in! Choose pass rates for metrics and assert they are satisfied
 # TODO: Fill me in! Add evaluation test that asserts pass rates are satisfied
